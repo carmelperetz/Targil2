3f63e7995c82b64b575fadd3bbb8628de352f611
dinba <dinba@DESKTOP-UH4LOS4>
2025-03-26 17:06:09 +0200

    change

--- Targil2Shitot/src/TestThreadCheckArray.java
+++ Targil2Shitot/src/TestThreadCheckArray.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 //Hi
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
@@ -6,11 +7,12 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<>();
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+				array.add(input.nextInt());
+				
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
@@ -37,7 +39,7 @@ public class TestThreadCheckArray {
 			}
 			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for(int index = 0; index < sd.getArray().size() ; index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
